a9d67eb661e9af109a75c6c6780f6c6f86420831
maxx2075 <mahmut_75@mail.ru>
2018-10-16 16:08:40 +0300

    Выполнение задания 5 и исправление задания 4

--- script.js
+++ script.js
@@ -38,8 +38,21 @@ for (i = 0; i <= 9; console.log(i++)) {
 }
     // **** Задание 4 ****
 console.log("**** Задание 4 ****");
-let pict = "x";
+let pict = "";
 for (i = 1; i <= 20; i++) {
-    console.log(pict);
     pict = pict + "x";
+    console.log(pict);    
 }
+
+    // **** Задание 5 ****
+console.log("**** Задание 5 ****");
+function countBasketPrice(basketfun) {
+    let summa = 0;    
+    for (let i=0; i < basketfun.length; i++) {
+     summa = summa + basketfun[i];   
+    }
+    return summa;
+}
+let basket = [23,34,56,364,754,345,745,75];
+console.log("Стоимость корзины " + countBasketPrice(basket));
+
